3e9564fac19c83e72e8164b9ca7d6cab7b54963a
ayennac <ayennac@gmail.com>
2022-11-14 18:11:29 -0800

    updated CardGroups

--- src/components/CardGroup.js
+++ src/components/CardGroup.js
@@ -4,7 +4,10 @@ const CardGroup=({title, cards, grid}) =>{
     return (
         <div class="card-deck">
         <div class="row">
-            <h3>Projects</h3>
+            <h3>{title}</h3>
+            <p>Add a filter function here with the tags: 
+                web development, project management, data analysis,
+                </p>
         {cards.map((card) => (<Card card={card} grid ={grid}/>))}
         </div>
         </div>
